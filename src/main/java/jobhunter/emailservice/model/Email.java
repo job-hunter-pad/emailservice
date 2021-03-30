ec16d4a58cca188abd558ba3241f520f4ef4679c
Ionut Iacob <ionut@Ubuntu20.04>
2021-03-30 16:50:37 +0300

    Created the model for the kafka message

---
+++ src/main/java/jobhunter/emailservice/model/Email.java
@@ -0,0 +1,28 @@
+package jobhunter.emailservice.model;
+
+public class Email {
+
+    private String email;
+    private String body;
+
+    public Email() {}
+
+    public Email(String email, String body) {
+        this.email = email;
+        this.body = body;
+    }
+
+    public String getEmail() { return email; }
+    public String getBody() { return body; }
+
+    public void setEmail(String email) { this.email = email; }
+    public void setBody(String body) { this.body = body; }
+
+    @Override
+    public String toString() {
+        return "Email{" +
+                "email='" + email + '\'' +
+                ", body='" + body + '\'' +
+                '}';
+    }
+}
